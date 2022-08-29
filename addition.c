78266721e8d0839b7a3fc6d0def55ac56cc39395
Turki-Alsuhibani <58737483+N7r-9@users.noreply.github.com>
2022-08-29 22:30:49 +0300

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	
+	int n1,n2,n3;
+	
+	printf("Enter the value for number 1 :");
+	
+	scanf("%d",&n1);
+	
+	printf("Enter the value for number 2 :");
+	
+	scanf("%d",&n2);
+	
+	n3 = n1 + n2;
+	
+	printf("The sum of %d+%d=%d",n1,n2,n3);
+	
+	return 0;
+	
+}
